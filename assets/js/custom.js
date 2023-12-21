1a1f4f14266a25a16a72b8c048d52759bd6b313b
jigar <khushbu@algoscript.in>
2023-12-21 13:43:33 +0530

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    $("#testimonial-slider").owlCarousel({
+      items: 3,
+      itemsDesktop:[1024,2],
+      itemsDesktopSmall:[979,2],
+      itemsTablet:[768, 2],
+      itemsMobile:[650, 1],
+      pagination: true,
+      autoPlay: true
+    });
+  });
+
+
+$(document).ready(function() {
+    $("#student-slider").owlCarousel({
+      items: 3,
+      itemsDesktop:[1024,3],
+      itemsDesktopSmall:[979,2],
+      itemsTablet:[768, 2],
+      itemsMobile:[650, 1],
+      pagination: true,
+      autoPlay: true
+    });
+  });
+
+
+$(document).ready(function() {
+    $("#rating-slider").owlCarousel({
+      items: 4,
+      itemsDesktop:[1024,4],
+      itemsDesktopSmall:[979,2],
+      itemsTablet:[768, 2],
+      itemsMobile:[650, 1],
+      pagination: true,
+      autoPlay: true
+    });
+  });
